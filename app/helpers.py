27a7b2149d06c5891d69b86a1e261dadbee04209
Luuk van den Beemt <luuk.beemt@gmail.com>
2023-04-25 15:29:53 +0200

    Moved credentials.json and token.json to /tokens folder

--- app/helpers.py
+++ app/helpers.py
@@ -21,18 +21,18 @@ def get_calender_service():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('/app/token.json'):
-        creds = Credentials.from_authorized_user_file('/app/token.json', SCOPES)
+    if os.path.exists('/tokens/token.json'):
+        creds = Credentials.from_authorized_user_file('/tokens/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '/app/credentials.json', SCOPES)
+                '/tokens/credentials.json', SCOPES)
             creds = flow.run_local_server(port=8080, bind_addr='0.0.0.0')
         # Save the credentials for the next run
-        with open('/app/token.json', 'w') as token:
+        with open('/tokens/token.json', 'w') as token:
             token.write(creds.to_json())
 
     return build('calendar', 'v3', credentials=creds)
