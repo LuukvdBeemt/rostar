d42d42d02d30f74f54574d8faab3b44acd1c893b
Luuk van den Beemt <luuk.beemt@gmail.com>
2023-05-01 21:17:39 +0200

    Allow for multiple classes

--- app/main.py
+++ app/main.py
@@ -1,55 +1,65 @@
 from helpers import *
+import csv
 import os
 
 
-WEEKS = 10
-
+WEEKS = 14
     
 def main():
+    # Get start day of current week
+    today = datetime.datetime.today().replace(microsecond=0).replace(tzinfo=ZoneInfo('Europe/Amsterdam'), hour=0, minute=0, second=0, microsecond=0)
+    start = today - datetime.timedelta(days=today.isoweekday() - 1)
+    
     # Get eduflex cookies and google calender service api
-    print("Getting eduflex cookies and calender service api...")
+    printDated("Getting eduflex cookies and calender service api...")
     sessionCookies = login(os.environ['ROSTAR_USER'], os.environ['ROSTAR_PASS'])
     service = get_calender_service()
 
-    # Get start day of current week
-    today = datetime.datetime.today().replace(tzinfo=ZoneInfo('Europe/Amsterdam'), hour=0, minute=0, second=0, microsecond=0)
-    start = today - datetime.timedelta(days=today.isoweekday() - 1)
+
+    # Get list of klassen
+    with open('/data/klassen.csv') as f:
+        csv_reader = csv.DictReader(f, delimiter=',')
+        for klas in csv_reader:
+            printDated(f"Processing {klas['name']}:")
+            processKlas(sessionCookies, service, start, klas['id'], klas['calenderId'])
     
+    
+def processKlas(sessionCookies, service, start, id, calenderId):
     for week in range(WEEKS):
-        print(f"Week {week}:")
+        printDated(f"Week {week}:")
 
         # The start and end of the current week
         timeMin = start + datetime.timedelta(weeks=week)
         timeMax = timeMin + datetime.timedelta(weeks=1)
 
-        print("Fetching upcoming google calender events...")
+        printDated("Fetching upcoming google calender events...")
         # Query all google calender items with a start time later than current time
-        events = service.events().list(calendarId='8f1fb85f4584ad953c5845069af48336ac78fb9eaa0013a1a4fa2004a2d11196@group.calendar.google.com', 
+        events = service.events().list(calendarId=calenderId, 
                 maxResults=2500, 
                 timeMin=timeMin.isoformat(), 
                 timeMax=timeMax.isoformat()
             ).execute().get('items', [])
 
         # Get Paralax appointments
-        print("Fetching upcoming eduflex appointments...")
-        appointments = getAppointments(sessionCookies, 5143, timeMin)
+        printDated("Fetching upcoming eduflex appointments...")
+        appointments = getAppointments(sessionCookies, id, timeMin)
 
-        print("Checking for new events...")
+        printDated("Checking for new events...")
         newEvents = []
         for appointment in appointments:
             newEvent = formatEvent(appointment)
             newEvents.append(newEvent)
 
             if not findEvent(newEvent, events):
-                event = service.events().insert(calendarId='8f1fb85f4584ad953c5845069af48336ac78fb9eaa0013a1a4fa2004a2d11196@group.calendar.google.com', body=newEvent).execute()    
-                print(f"Created calender item with id: {event['id']}")
-                # print(f"Created calender item with id")
+                event = service.events().insert(calendarId=calenderId, body=newEvent).execute()    
+                printDated(f"Created calender item with id: {event['id']}")
+                # printDated(f"Created calender item with id")
 
-        print("Checking for deleted events...")
+        printDated("Checking for deleted events...")
         for event in events:
             if not findEvent(event, newEvents):
-                print(f"Deleting calender item with id: {event['id']}")
-                event = service.events().delete(calendarId='8f1fb85f4584ad953c5845069af48336ac78fb9eaa0013a1a4fa2004a2d11196@group.calendar.google.com', eventId=event["id"]).execute()    
+                printDated(f"Deleting calender item with id: {event['id']}")
+                event = service.events().delete(calendarId=calenderId, eventId=event["id"]).execute()    
 
 
 def findEvent(newEvent, events):
@@ -73,6 +83,10 @@ def locationsMatch(newEvent, event):
             return True
             
     return False
+
+def printDated(message):
+    now = datetime.datetime.today().replace(microsecond=0)
+    print(f"{now.isoformat()}: {message}")
 
 
 if __name__ == '__main__':
